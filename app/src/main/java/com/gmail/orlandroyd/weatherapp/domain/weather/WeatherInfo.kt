ec19f3e225614f6ffb51e62c17f9b572bd97b4f2
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-07-06 16:50:05 -0400

    WeatherInfo.kt and WeatherData.kt create (domain models)

---
+++ app/src/main/java/com/gmail/orlandroyd/weatherapp/domain/weather/WeatherInfo.kt
@@ -0,0 +1,6 @@
+package com.gmail.orlandroyd.weatherapp.domain.weather
+
+data class WeatherInfo(
+    val weatherDataPerDay: Map<Int, List<WeatherData>>,
+    val currentWeatherData: WeatherData?,
+)
