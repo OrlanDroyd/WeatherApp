cc4cb7c85dfac390cb9c32465bd477ac2e9974d1
orlando.ppena <orlando.ppena@reduc.edu.cu>
2022-07-06 17:26:22 -0400

    Dagger Hilt setup

---
+++ app/src/main/java/com/gmail/orlandroyd/weatherapp/di/LocationModule.kt
@@ -0,0 +1,20 @@
+package com.gmail.orlandroyd.weatherapp.di
+
+import com.gmail.orlandroyd.weatherapp.data.location.DefaultLocationTracker
+import com.gmail.orlandroyd.weatherapp.domain.location.LocationTracker
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class LocationModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindLocationTracker(defaultLocationTracker: DefaultLocationTracker): LocationTracker
+}
---
+++ app/src/main/java/com/gmail/orlandroyd/weatherapp/di/RepositoryModule.kt
@@ -0,0 +1,22 @@
+package com.gmail.orlandroyd.weatherapp.di
+
+import com.gmail.orlandroyd.weatherapp.data.repository.WeatherRepositoryImpl
+import com.gmail.orlandroyd.weatherapp.domain.repository.WeatherRepository
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import javax.inject.Singleton
+
+@ExperimentalCoroutinesApi
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    @Singleton
+    abstract fun bindWeatherRepository(
+        weatherRepositoryImpl: WeatherRepositoryImpl,
+    ): WeatherRepository
+}
